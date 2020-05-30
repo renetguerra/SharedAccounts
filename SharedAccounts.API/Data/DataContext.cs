8ea191b54da7bcb0d2d21876abed3bf3f78b5afc
Rene TG <renetguerra@gmail.com>
2020-05-30 19:30:15 +0200

    SharedAccounts Initial

---
+++ SharedAccounts.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SharedAccounts.API.Models;
+
+namespace SharedAccounts.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        
+        public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SharedAccounts.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using SharedAccounts.API.Models;
+
+namespace SharedAccounts.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
